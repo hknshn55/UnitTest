b68ae0538ca20245a9d4c135f52488528b4cf524
Hakan Şahin <hknshn55@hotmail.com>
2022-08-04 15:15:35 +0300

    Exeption karşısında testin doğru sonuc vermesi. Hatayı teste tanıtmak.

---
+++ ExamplePlay/HataTepki.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamplePlay
+{
+    public static class HataTepki
+    {
+        //Bir metodumuz olsun ve bu metodun işlemlerinden biri hata döndürmek olsun.
+        //Bu metod üzerinde bilerek hata verdirdiğimizde test ten başarısız sonucunu alırız.
+        //Yani bilerek verdirdiğimiz hatayı tanıtmamız gereklidir. Burada hata vermesi normal
+        //şeklinde bildirmemiz gerekiyor.
+
+        public static bool Hata()
+        {
+            bool value = false;
+            if (value)
+            {
+                return value;
+            }
+            throw new Exception($"{value} true olmalıdır.");
+        }
+    }
+}
---
+++ UnitTestProject1/HataTepkiTests.cs
@@ -0,0 +1,30 @@
+﻿using ExamplePlay;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class HataTepkiTests
+    {
+        [TestMethod]
+        //Exeption hataların atası olduğu için tek başına bir hata olarak belirtilemez.
+        //Eger Exeption olarak gelen hatada tipini özel olarak bulmasını istiyorsak AllowDerivedTypes özelliğini
+        //true haline getiririz.
+        [ExpectedException(typeof(Exception), AllowDerivedTypes = true)] //=>Burada gelen hatayı otomatik tanıttık ve testi geçirdik.
+
+        //Bizzat hangi hata olduğunu belirttiğimizde sadece o hatayı tanıyacaktır ve testi başarı ile
+        // geçecektir. ArgumentExeption, Exeption türlerinden 1i olduğu için bu şekilde kabul edecektir.
+        // [ExpectedException(typeof(ArgumentException))]
+        public void TM1()
+        {
+            bool beklenen = true;
+            bool gelen = HataTepki.Hata();
+            Assert.AreEqual(beklenen,gelen);
+        }
+    }
+}
